e29dce591a97215cd4c371774a5405979f9ef5a1
Valerii Radchenko <valerii.radchenko@akvelon.com>
2017-06-18 13:16:00 +0300

    added error handling on monitoring draw

--- canary/monitoring/monitoring.js
+++ canary/monitoring/monitoring.js
@@ -9,7 +9,7 @@ const stat = util.promisify(pidusage.stat);
 class Monitoring {
 
     constructor(showMonitoring) {
-        this.processRegestry = {};
+        this.processRegistry = {};
 
         this.cliView = (showMonitoring) ?
             new CLIView(['pid', 'cpu', 'memory'], config.monitoring.columnWidth) :
@@ -19,17 +19,17 @@ class Monitoring {
     }
 
     add(osProcess) {
-        this.processRegestry[osProcess.pid] = osProcess;
+        this.processRegistry[osProcess.pid] = osProcess;
 
         this.cliView && this.cliView.push([osProcess.pid, 0, 0]);
 
         osProcess.on('close', processData => {
             this.cliView && this.cliView.remove(processData.pid);
             pidusage.unmonitor(processData.pid);
-            delete this.processRegestry[processData.pid];
+            delete this.processRegistry[processData.pid];
         });
 
-        if (Object.keys(this.processRegestry).length === 1) {
+        if (Object.keys(this.processRegistry).length === 1) {
             this.monitor()
                 .then()
                 .catch(console.error);
@@ -37,20 +37,26 @@ class Monitoring {
     }
 
     async monitor() {
-        if (Object.keys(this.processRegestry).length < 1) {
+        if (Object.keys(this.processRegistry).length < 1) {
             return false;
         }
 
-        for (let pid in this.processRegestry) {
-            let osProcessUsage = await stat(pid);
-            osProcessUsage.memory = +(osProcessUsage.memory / helpers.getMemoryMeasure(this.memoryUnit)).toFixed(2);
-            osProcessUsage.cpu = +(osProcessUsage.cpu.toFixed(2));
+        for (let pid in this.processRegistry) {
+            let osProcessUsage = {cpu: 0, memory: 0};
 
-            this.cliView && this.cliView.push([
-                pid,
-                osProcessUsage.cpu,
-                `${osProcessUsage.memory} ${helpers.getShortBiteUnitName(this.memoryUnit)}`
-            ]);
+            try {
+                osProcessUsage = await stat(pid);
+                osProcessUsage.memory = +(osProcessUsage.memory / helpers.getMemoryMeasure(this.memoryUnit)).toFixed(2);
+                osProcessUsage.cpu = +(osProcessUsage.cpu.toFixed(2));
+
+                this.cliView && this.cliView.push([
+                    pid,
+                    osProcessUsage.cpu,
+                    `${osProcessUsage.memory} ${helpers.getShortBiteUnitName(this.memoryUnit)}`
+                ]);
+            } catch (error) {
+
+            }
 
         }
 
